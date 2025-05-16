1c2fb4b16be8a7693288e08253ba2d2352c4167d
Jean <jeancarlos12762@gmail.com>
2025-05-16 10:09:07 -0300

    Alterando imagens e centralizando CSS dos carros

--- src/js/script.js
+++ src/js/script.js
@@ -1,20 +1,20 @@
-let imagens =[
-    'src/assets/imagem.home1.avif',
-    'src/assets/imagem.home2.avif',
-    'src/assets/imagem.home3.avif',
-    ]
-    
-    let i = 0;
-    let tempo = 5000;
-    
- 
-    
-    function slideShow(){
-        document.getElementById('image').src = imagens[i];
-        i++;
-        if(i == imagens.length){
-            i=0;
-        }
-        setTimeout("slideShow()",tempo)
+let imagens = [
+    'src/assets/imagem.home1.jpg',
+    'src/assets/imagem.home2.jpg',
+    'src/assets/imagem.home3.jpg',
+]
+
+let i = 0;
+let tempo = 5000;
+
+
+
+function slideShow() {
+    document.getElementById('image').src = imagens[i];
+    i++;
+    if (i == imagens.length) {
+        i = 0;
     }
-    slideShow();
+    setTimeout("slideShow()", tempo)
+}
+slideShow();

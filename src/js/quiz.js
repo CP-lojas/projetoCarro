3b0f008cfda4a00ef708322a12e497715334b1ee
Enrico <rico.set06@gmail.com>
2025-05-16 09:39:36 -0300

    Finalizando gabarito

--- src/js/quiz.js
+++ src/js/quiz.js
@@ -25,6 +25,7 @@ window.addEventListener('DOMContentLoaded', () => {
         "Quanto tempo leva, em média, para carregar totalmente um carro elétrico em uma tomada comum?",
         "Qual é a principal vantagem dos carros elétricos em relação aos carros a combustão?",
         "Os carros elétricos são mais silenciosos do que os carros a combustão?",
+        "Os carros elétricos têm a mesma autonomia que os carros a combustão?",
     ]
     //DECLARANDOS AS VARIAVEIS
     let perguntas = 0;
@@ -80,75 +81,8 @@ window.addEventListener('DOMContentLoaded', () => {
 })
 
 
-// GABARITO
+function mostrarGabarito(){
+    const gabaritoDiv = document.getElementById("gabarito");
+    gabaritoDiv.style.display = "block";
 
-document.addEventListener('DOMContentLoaded',()=>{
-    const mensagem = document.getElementById('message');
-    const containerResultado = document.getElementById('container-resultado');
-    const mostreGabarito = document.getElementById("mostre");
-    const listaResultado = document.getElementById('lista-resultado');
-    const reiniciarBotao = document.getElementById('inicio-btn')
-
-    //DECLARANDO O ARRAY DE PERGUNTAS
-    const gabarito=[
-        "O que move um carro elétrico? ",
-        "Qual componente é responsável por armazenar energia em um carro elétrico?",
-        "Como os carros elétricos são recarregados?",
-        "Qual é o principal tipo de motor usado em carros elétricos?",
-        "Carros elétricos emitem CO₂ pelo escapamento?",
-        "O que é um carro híbrido?",
-        "Quanto tempo leva, em média, para carregar totalmente um carro elétrico em uma tomada comum?",
-        "Qual é a principal vantagem dos carros elétricos em relação aos carros a combustão?",
-        "Os carros elétricos são mais silenciosos do que os carros a combustão?",
-    ]
-    //DECLARANDOS AS VARIAVEIS
-    const respostas = [];
-
-    function mostrarPergunta(){
-        if(perguntas <questoes.length){
-            pergunta.textContent =gabarito[perguntas];
-            resposta.value='';
-            mensagem.textContent ='';
-        }else{
-            mostrarResultado();
-        }
-    }
-
-    function mostrarResultado(){
-        containerPerguntas.classList.add('hidden');
-        containerResultado.classList.remove('hidden');
-        listaResultado.innerHTML='';
-
-        gabarito.forEach((questoes,index)=>{
-            const listaItem =document.createElement('li');
-            listaItem.innerHTML = `<strong>${gabarito}</strong><br>
-            Sua Resposta: <span>${respostas[index]}</span>`
-            listaResultado.appendChild(listaItem);
-        })
-    }
-
-    function Mostrargabarito(){
-        const respostaAtual =resposta.value.trim();
-        if(respostaAtual ===''){
-            mensagem.textContent ="Por favor , digite sua resposta";
-            return;
-        }
-        respostas.push(respostaAtual);
-        perguntas++;
-        mostrarPergunta();
-
-    }
-    function reiniciarQuiz(){
-        perguntas =0;
-        respostas.length =0;
-        containerResultado.classList.add('hidden');
-        containerPerguntas.classList.remove('hidden')
-        mostrarPergunta();
-    }
-
-    proximaPergunta.addEventListener('click', Mostrargabarito);
-    reiniciarBotao.addEventListener('click',reiniciarQuiz);
-
-    mostrarPergunta();
-
-})
+}
